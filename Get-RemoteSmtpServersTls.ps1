e96f597d2cb0d8ccf4d41672d632ef9d538be729
Torsten Schlopsnies <26271212+xedoc64@users.noreply.github.com>
2021-04-12 15:22:31 +0200

    Overhauled the file reading and Regex. Using Streamreader and .NET Regex now

--- Get-RemoteSmtpServersTls.ps1
+++ Get-RemoteSmtpServersTls.ps1
@@ -35,31 +35,28 @@
     .PARAMETER Backend
     Search backend transport (aka hub transport) log files, instead of frontend transport, which is the default
 
-    .PARAMETER ToCsv
-    Export search results to a single CSV file for all servers
-
-    .PRAMATER ToCsvPerServer
+    .PARAMETER ToCsvPerServer
     Export search results to a separate CSV file per servers
 
     .PARAMETER AddDays
     File selection filter, -5 will select log files changed during the last five days. Default: -10
 
-  
-    .EXAMPLE
-    .\Get-RemoteSmtpServers.ps1 -Servers SRV01,SRV02 -LegacyExchange -AddDays -4 -ToCsv
+    .PARAMETER ResolveNames
+    Resolve the ip addresses to names (if possible)
 
-    Search legacy Exchange servers SMTP receive log files for the last 4 days and save search results in a single CSV file
+    .EXAMPLE
+    .\Get-RemoteSmtpServersTLS.ps1 -Servers SRV01,SRV02 -AddDays -4 -ToCsvPerServer -ResolveNames
    
 #>
 
 
 [CmdletBinding()]
 param(
-  $Servers = @('MYEXCHANGE'),
+  $Servers = @('localhost'),
   [switch]$Backend,
-  [switch]$ToCsv,
   [switch]$ToCsvPerServer,
-  [int]$AddDays = -10
+  [int]$AddDays = -10,
+  [switch]$ResolveNames
 )
 
 $CsvFileName = ('RemoteSMTPServersTls-%SERVER%-%ROLE%-%TLS%-{0}.csv' -f ((Get-Date).ToString('s').Replace(':','-')))
@@ -74,50 +71,48 @@ $TlsProtocols = @('TLS1_2','TLS1_1','TLS1_0')
 
 # The SMTP receive log search pattern
 $Pattern = '(.)*SP_PROT_%TLS%(.)*succeeded'
+$PreStagePattern = [System.Text.RegularExpressions.Regex]('(.)*SP_PROT_TLS(.)*succeeded')
 
-# An empty array for storing remote servers
-$RemoteServers = @()
-
-# Create an empty array
-$RemoteServersOutput = @()
 
 function Write-RemoteServers {
   [CmdletBinding()]
   param(
-    [string]$FilePath = ''
+    [string]$FilePath = '',
+    [hashtable]$ResultsToExport
+    
   )
-
-  # sort servers
-  $RemoteServers = $RemoteServers | Select-Object -Unique | Sort-Object
-
-  if(($RemoteServers| Measure-Object).Count -gt 0) {
-    
-    # Create an empty array
-    $RemoteServersOutput = @()
-
-    foreach($Server in $RemoteServers) { 
-    
-      if($Server.Trim() -ne '') { 
-        $obj = New-Object -TypeName PSObject
-        $obj | Add-Member -MemberType NoteProperty -Name 'Remote Server' -Value $Server
-        $RemoteServersOutput += $obj
+    $RemoteServersOutput = New-Object System.Collections.Generic.List[System.Object]
+    # Looping though the hashtable(s)
+    foreach($Server in $ResultsToExport.Keys) {
+      foreach($Tls in $TlsProtocols) {
+        $RemoteServersOutput.Clear()
+        if ($ResultsToExport.$Server.$Tls.Count -ge 1) {
+          for($i=0; $i -lt $ResultsToExport.$Server.$Tls.Count; $i++) {
+            if ($ResolveNames) {
+              # Resolving the ip
+              $HostName = Resolve-DnsName -Name $ResultsToExport.$Server.$Tls[$i] -ErrorAction Ignore | Select-Object -ExpandProperty NameHost -ErrorAction Ignore
+              if ($null -ne $HostName) {
+                $ResultsToExport.$Server.$Tls[$i] = $HostName
+              }
+            }
+            # Add the result to a generic list for export
+            $RemoteServersOutput.Add((New-Object -TypeName "psobject" -Property @{"Remote server"="$($ResultsToExport.$Server.$Tls[$i])"})) 
+          }
+          if($ToCsvPerServer) {
+            # save remote servers list as csv
+            $CSV = $FilePath.Replace('%TLS%',$Tls)
+            if (-not (Test-Path -Path $FilePath)) {
+              $null = $RemoteServersOutput | Export-Csv -Path $CSV -Encoding UTF8 -NoTypeInformation -Force -Confirm:$false -NoClobber
+              Write-Verbose -Message ('Remote server list written to: {0}' -f $FilePath)
+            }
+          }
+          Write-Host "Server: $($Server)"
+          Write-Host "Protocoll: $($Tls)"
+          Write-Host "Remote servers:"
+          $RemoteServersOutput
+        }
       }
     }
-
-    if($ToCsv -or $ToCsvPerServer) {
-      # save remote servers list as csv
-      $null = $RemoteServersOutput | Export-Csv -Path $FilePath -Encoding UTF8 -NoTypeInformation -Force -Confirm:$false
-
-      Write-Verbose -Message ('Remote server list written to: {0}' -f $FilePath)
-    }
-
-    $RemoteServersOutput
-  
-  }
-  else {
-    Write-Host 'No remote servers found!' 
-  }
-
 }
 
 ## MAIN ###########################################
@@ -136,8 +131,13 @@ Write-Verbose -Message ('CsvFileName: {0}' -f ($CsvFileName))
 
 # Fetch each Exchange Server server 
 foreach($Server in $Servers) {
- 
+  
   $Server = $Server.ToUpper()
+
+  # Lists and Hashtables
+  $PreStageResult = New-Object System.Collections.Generic.List[string]
+  $Results = @{}
+  $Results["$($Server)"] = @{}
 
   $Path = $LogPath.Replace('%SERVER%', $Server)
 
@@ -145,70 +145,50 @@ foreach($Server in $Servers) {
 
   # fetching log files requires an account w/ administrative access to the target server
   $LogFiles = Get-ChildItem -Path $Path -File | Where-Object {$_.LastWriteTime -gt (Get-Date).AddDays($AddDays)}
+  
 
   $LogFileCount = ($LogFiles | Measure-Object).Count
-  
-  foreach($Tls in $TlsProtocols) {
-    Write-Host ('Working on: {0}' -f $Tls)
-    $FileCount = 1
-
-    foreach($File in $LogFiles) {
-
-      Write-Progress -Activity ('{3} | {4} | File [{0}/{1}] : {2}' -f $FileCount, $LogFileCount, $File.Name, $Server, $Tls) -PercentComplete(($FileCount/$LogFileCount)*100)
-
- 
-      # find results in selected log files
-      $SearchPattern = $Pattern.Replace('%TLS%', $Tls)
-
-      $results = (Select-String -Path $File.FullName -Pattern $SearchPattern)
-
-      Write-Verbose -Message ('Results {0} : {1}' -f $File.FullName, ($results | Measure-Object).Count)
-
-      # Get remote server information from search string result
-      foreach($record in $results) {
-        
-        # Fetch remote hostname
-        # $HostName = ($record.line -replace $Pattern,'').Replace(',','').Trim().ToUpper()
-        $HostIp = (($record.Line).Split(',')[5]).Split(':')[0]
-
-        # Try to resolve remote IP address as the line does not contain a server name
-        $HostName = Resolve-DnsName $HostIp -ErrorAction Ignore |Select-Object -ExpandProperty NameHost -ErrorAction Ignore
-
-        if ($null -ne $HostName)
-        {
-          if(-not $RemoteServers.Contains($HostName)) { 
-            $RemoteServers += $HostName
+  $FileCount = 1
+  foreach($File in $LogFiles) {
+    # looping through the file and store matched line into the generic list. We will use the list afterwards to check against the protocol
+    # pattern.
+    # We use .NET regex here
+    if (Test-Path -Path $File.FullName) {
+      try {
+        $PreStagedFile = [System.IO.StreamReader]($File.FullName)
+        while ($PreStagedFile.EndOfStream -eq $false) {
+          $line = $PreStagedFile.ReadLine()
+          $match = $PreStagePattern.Match($line)
+          if ($match.Success -eq $true) {
+            $PreStageResult.Add($line)
           }
         }
-        else {
-          if(-not $RemoteServers.Contains($HostIp)) { 
-            $RemoteServers += $HostIp
-          }
-        }
-        
       }
-
-      $FileCount++
-
+      catch {
+        Write-Verbose "File $($File.Fullname) couldn't be read. Maybe the file is open by another process."
+      }
+      $PreStagedFile.Dispose()
     }
 
-    if($ToCsvPerServer) {
-   
-      $CsvFile = $CsvFileName.Replace('%SERVER%',$Server).Replace('%TLS%',$Tls)
+    foreach($Tls in $TlsProtocols) {
+      Write-Progress -Activity ('{3} | {4} | File [{0}/{1}] : {2}' -f $FileCount, $LogFileCount, $File.Name, $Server, $Tls) -PercentComplete(($FileCount/$LogFileCount)*100)
+      # Nested generic list inside of a hash table
+      $Results["$($Server)"]["$($Tls)"] = New-Object System.Collections.Generic.List[string]
 
-      Write-Verbose -Message $CsvFile
-
-      Write-RemoteServers -FilePath $CsvFile
-    
-      $RemoteServers = @()
+      $SearchPattern = [System.Text.RegularExpressions.Regex]("$($Pattern.Replace('%TLS%', $Tls))")
+      foreach($line in $PreStageResult) {
+        $match = $SearchPattern.Match($line)
+        if ($match.Success -eq $true) {
+          $HostIP = (($line).Split(',')[5]).Split(':')[0]
+          if (-not ($Results["$($Server)"]["$($Tls)"].Contains($HostIP))) {
+            $Results["$($Server)"]["$($Tls)"].Add($HostIP)
+          }
+        }
+      }
     }
+    $FileCount++
   }
-}
 
-if($ToCsv) { 
-  $CsvFile = $CsvFileName.Replace('%SERVER%','ALL')
-
-  Write-Verbose -Message $CsvFile
-
-  Write-RemoteServers -FilePath $CsvFile
+  $CsvFile = $CsvFileName.Replace('%SERVER%',$Server)
+  Write-RemoteServers -FilePath $CsvFile -ResultsToExport $Results
 }
